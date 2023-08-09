988ab30823fd3b8c0dbbad4d7c8a95f4d00476c0
Ben Hensor <benhensor@gmail.com>
2023-08-09 11:01:06 +0100

    boiler plate code complete

---
+++ src/components/produceTracker/ProduceTracker.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './producetracker.css';
+
+
+const ProduceTracker = () => {
+
+
+  return (
+      <section>
+        <div className="container">
+          <h1>Seasonal Produce Finder</h1>
+          <h3>Find out which foods are in season</h3>
+
+          <form>
+            <button className="january">January</button>
+            <button className="february">February</button>
+            <button className="march">March</button>
+            <button className="april">April</button>
+            <button className="may">May</button>
+            <button className="june">June</button>
+            <button className="july">July</button>
+            <button className="august">August</button>
+            <button className="september">September</button>
+            <button className="october">October</button>
+            <button className="november">November</button>
+            <button className="december">December</button>
+          </form>
+
+          <div className="display">
+
+
+
+          </div>
+        </div>
+      </section>
+  )
+}
+
+export default ProduceTracker;
